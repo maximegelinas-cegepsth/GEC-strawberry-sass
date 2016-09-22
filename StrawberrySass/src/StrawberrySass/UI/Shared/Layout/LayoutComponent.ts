5f49dfd3bc1f32d41fbc7fdcd8a4f3da65b5cb75
maximegelinas <maxime.gelinas@idmobiles.com>
2016-09-22 18:03:26 -0400

    Add layout styles

--- StrawberrySass/src/StrawberrySass/UI/Shared/Layout/LayoutComponent.ts
+++ StrawberrySass/src/StrawberrySass/UI/Shared/Layout/LayoutComponent.ts
@@ -3,9 +3,19 @@
 @Component({
     moduleId: module.id,
     selector: 'app-layout',
-    templateUrl: 'LayoutComponent.html'
+    templateUrl: 'LayoutComponent.html',
+    styleUrls: [
+        'ToolbarComponent.css',
+        'SidenavComponent.css'
+    ]
 })
 export class LayoutComponent {
+
+    links: {}[] = [
+        { url: '/welcome', text: 'Accueil' },
+        { url: '/about', text: 'À propos' },
+        { url: '/contact-us', text: 'Nous joindre' }
+    ];
 
     // TODO(maximegelinas): Gets the toolbar height dynamically.
     toolbarHeight = '64px';
