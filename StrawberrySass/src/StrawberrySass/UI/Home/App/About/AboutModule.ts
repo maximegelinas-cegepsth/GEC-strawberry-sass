1f1866e31fa68b1fdcd35144e6142de3e170a990
maximegelinas <maxime.gelinas@idmobiles.com>
2016-09-22 20:56:39 -0400

    Style About page

--- StrawberrySass/src/StrawberrySass/UI/Home/App/About/AboutModule.ts
+++ StrawberrySass/src/StrawberrySass/UI/Home/App/About/AboutModule.ts
@@ -1,12 +1,20 @@
 ﻿import { NgModule } from '@angular/core';
 import { CommonModule }   from '@angular/common';
 
+import { MdButtonModule } from '@angular2-material/button';
+import { MdIconModule } from '@angular2-material/icon';
+import { MdListModule } from '@angular2-material/list';
+
 import { AboutComponent } from './AboutComponent';
 import { routing } from './AboutRouting';
 
 @NgModule({
     imports: [
         CommonModule,    
+
+        MdButtonModule.forRoot(),
+        MdIconModule.forRoot(),
+        MdListModule.forRoot(),
 
         routing
     ],
